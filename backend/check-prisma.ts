a98743b2f0d52ab7997c2f80548267e61f8dd8cf
kapsi1 <kapsiakk@gmail.com>
2026-03-06 19:59:35 +0100

    code review

--- backend/check-prisma.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-console.log('User fields:', Object.keys((prisma as any).user.fields || {}));
-console.log('Subscription fields:', Object.keys((prisma as any).subscription.fields || {}));
-console.log('Alert fields:', Object.keys((prisma as any).alert.fields || {}));
-process.exit(0);
